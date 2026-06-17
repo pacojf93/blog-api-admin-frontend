9e8dc6266d7e76c9fae4a90d9cc38da61a7639c5
indumatic <electrico@indumatic.net>
2026-06-17 10:45:58 +0200

    add post edition functionality

---
+++ src/components/posts/PostEditor.jsx
@@ -0,0 +1,74 @@
+import { useState, useRef, useEffect } from 'react'
+import { useOutletContext, useParams } from 'react-router'
+import Tinymce from '../Editor'
+
+const savePost = (post, user, content) => {
+  const headers =
+    user && user.token ? { Authorization: `Bearer ${user.token}` } : {}
+
+  fetch(`/api/posts/${post.id}`, {
+    method: 'PUT',
+    headers: headers,
+    body: new URLSearchParams({
+      id: post.id,
+      title: post.title,
+      content: content,
+    }),
+  })
+    .then((response) => response.json())
+    .then((res) => console.log(res))
+}
+
+const PostEditor = () => {
+  const { id } = useParams()
+  const { user, navigate } = useOutletContext()
+  const [post, setPost] = useState(null)
+  const [content, setContent] = useState('')
+  const editorRef = useRef(null)
+
+  useEffect(() => {
+    fetch(`/api/posts/${id}`, {})
+      .then((res) => res.json())
+      .then((res) => {
+        setPost(res)
+        setContent(res.content)
+      })
+  }, [id])
+
+  return (
+    <>
+      {post ? (
+        <>
+          <div className='mt-5'>
+            <Tinymce
+              editorRef={editorRef}
+              initialValue={post.content}
+              setContent={setContent}
+            />
+          </div>
+
+          <button
+            onClick={() => {
+              savePost(post, user, content)
+              navigate(-1)
+            }}
+            className='mt-5 btn btn-outline-primary'
+          >
+            Save editor content
+          </button>
+          <div className='mt-5'>
+            <h2>Preview</h2>
+            <div
+              dangerouslySetInnerHTML={{ __html: content }}
+              className='mt-5'
+            />
+          </div>
+        </>
+      ) : (
+        <p>no post available</p>
+      )}
+    </>
+  )
+}
+
+export default PostEditor
--- src/components/posts/PostsRoute.jsx
+++ src/components/posts/PostsRoute.jsx
@@ -2,12 +2,14 @@ import { Route } from 'react-router'
 import Posts from './Posts'
 import PostList from './PostList'
 import PostDetail from './PostDetail'
+import PostEditor from './PostEditor'
 
 const PostsRoute = (
   <Route path='posts' element={<Posts />}>
     <Route index element={<PostList />} />
     <Route path='new' element={<h1>nu post</h1>} />
     <Route path=':id' element={<PostDetail />} />
+    <Route path=':id/edit' element={<PostEditor />} />
   </Route>
 )
 
